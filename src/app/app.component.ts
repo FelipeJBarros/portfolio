caa91a7079ac8191ad79a470b22869d2b2433bed
Felipe Jonathan <felipejonathan.dev@gmail.com>
2024-02-27 18:53:57 -0300

    feat: creating contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { NgOptimizedImage } from '@angular/common';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ContactsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
