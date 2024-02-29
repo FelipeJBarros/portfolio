4faa4ca9cfa03b455ad32bcd93fbbe24cb4759c9
Felipe Jonathan <felipejonathan.dev@gmail.com>
2024-02-29 10:35:47 -0300

    refactor: updating icons from contact

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { LinkedinComponent } from '../icons/linkedin/linkedin.component';
+import { GithubComponent } from '../icons/github/github.component';
+import { InstagramComponent } from '../icons/instagram/instagram.component';
+import { MailComponent } from '../icons/mail/mail.component';
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [],
+  imports: [
+    LinkedinComponent,
+    GithubComponent,
+    InstagramComponent,
+    MailComponent
+  ],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
